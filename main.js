20450beb8d90db429a0bf6a27c97de205201e9c0
calebcoding12 <107775348+calebcoding12@users.noreply.github.com>
2022-11-13 14:12:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+nose_x = 0;
+nose_y = 0;
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function draw(){
+    image(video,0,0,300,300);
+
+}
+
+function take_snapshot(){
+    save("myfilterimage.png");
+}
+function modelLoaded(){
+    console.log("Model is loaded");
+}
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+        nose_x = results[0].pose.nose.x - 25;
+        nose_y = results[0].pose.nose.y - 12;
+        console.log("nose x = " + nose_x);
+        console.log("nose y = " + nose_y);
+    }
+}
